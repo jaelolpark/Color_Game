edcfc8366a75b4d369bf528e35b62df3a2e34014
jaelolpark <ac2cic@gmail.com>
2020-04-22 20:15:13 -0400

    css for button, squares and so on

--- Color_Game.js
+++ Color_Game.js
@@ -54,7 +54,7 @@ resetBtn.addEventListener('click', function() {
   for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = '#232323';
+  h1.style.backgroundColor = 'steelblue';
 })
 
 // =================================== Color quiz display
